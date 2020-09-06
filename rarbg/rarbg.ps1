fef611d7a81f532b67a6d0a99926f19e566d7f05
CatTheHacker <me@catthehacker.com>
2020-09-06 10:47:28 +0000

    Add colours! And change ArrayList to Generic List

--- rarbg/rarbg.ps1
+++ rarbg/rarbg.ps1
@@ -5,16 +5,16 @@ $i = 1
 $WaitTime = 7
 # ? Cookie for anti-script security
 $Headers = @{'Cookie' = ''}
-$Torrents = [System.Collections.ArrayList]::New()
+$Torrents = [System.Collections.Generic.List[string]]::New()
 $ProgressPreference = 'SilentlyContinue'
 do {
-    [System.Console]::WriteLine('Collecting links for ' + $query + ', page ' + $i)
+    [System.Console]::WriteLine("`e[38;5;11m Collecting links for `e[38;5;9m" + $query + "`e[38;5;11m, page `e[38;5;9m" + $i + "`e[0m")
     $page = Invoke-WebRequest ('https://rarbgp2p.org/torrents.php?search=' + $query + '&page=' + $i) -Headers $Headers -UserAgent ([Microsoft.PowerShell.Commands.PSUserAgent]::Chrome)
     $TorrentsTemp = ($page.links | Where-Object {$_.href -like '/torrent/*'} | Select-Object href).href -replace '#comments','' | Sort-Object -Unique
     $NextPage = ($page.links | Where-Object {$_.title -eq 'next page'} | Select-Object href).href -replace 'amp;','' | Sort-Object -Unique
 
     ForEach($Torrent in $TorrentsTemp){
-        [System.Console]::WriteLine('Adding ' + $Torrent)
+        [System.Console]::WriteLine("`e[38;5;11m Adding `e[38;5;9m" + $Torrent + "`e[0m")
         [void]$Torrents.Add($Torrent)
     }
 
@@ -32,22 +32,22 @@ do {
 } while ($true)
 
 ForEach($Torrent in $Torrents){
-    [System.Console]::WriteLine('Downloading torrent file for ' + ($Torrent -split '/')[2])
+    [System.Console]::WriteLine("`e[38;5;11m Downloading torrent file for `e[38;5;9m" + ($Torrent -split '/')[2] + "`e[0m")
     $Page = Invoke-WebRequest ('https://rarbgp2p.org' + $Torrent) -Headers $Headers -UserAgent ([Microsoft.PowerShell.Commands.PSUserAgent]::Chrome)
 
     $TorrentFile = $page.links | Where-Object {$_.href -like '/download.php*'}
     $TorrentFileName = ($TorrentFile.href -split '=')[($TorrentFile.href -split '=').Count - 1]
 
-    [System.Console]::WriteLine('Found ' + $TorrentFileName + ', url ' + $TorrentFile.href)
+    [System.Console]::WriteLine("`e[38;5;11m Found `e[38;5;9m" + $TorrentFileName + "`e[38;5;11m, url `e[38;5;9m" + $TorrentFile.href + "`e[0m")
 
     For($r = $WaitTime;$r -gt 0; $r--){
         [System.Console]::Write('.')
         [System.Threading.Thread]::Sleep(1000)
     }
 
-    [System.Console]::WriteLine('Downloading ' + $TorrentFileName + ', id: ' + ($Torrent -split '/')[2])
+    [System.Console]::WriteLine("`e[38;5;11m Downloading `e[38;5;9m" + $TorrentFileName + "`e[38;5;11m, id: `e[38;5;9m" + ($Torrent -split '/')[2] + "`e[0m")
     Invoke-WebRequest ('https://rarbgp2p.org' + $TorrentFile.href) -OutFile $TorrentFileName -Headers $Headers -UserAgent ([Microsoft.PowerShell.Commands.PSUserAgent]::Chrome)
-    [System.Console]::WriteLine('Finished downloading ' + $TorrentFileName)
+    [System.Console]::WriteLine("`e[38;5;2m Finished downloading `e[38;5;3m" + $TorrentFileName + "`e[0m")
 
     For($r = $WaitTime;$r -gt 0; $r--){
         [System.Console]::Write('.')
