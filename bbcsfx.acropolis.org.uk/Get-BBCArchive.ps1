14dc206e8c1a6874e42b8cfe04ba5f15dbb6d56a
CatTheHacker <me@catthehacker.com>
2020-09-25 11:20:54 +0000

    parallel

--- bbcsfx.acropolis.org.uk/Get-BBCArchive.ps1
+++ bbcsfx.acropolis.org.uk/Get-BBCArchive.ps1
@@ -3,9 +3,16 @@ function Get-BBCArchive {
     $Root = (Get-Location).Path
     $WebClient.DownloadFile('http://bbcsfx.acropolis.org.uk/assets/BBCSoundEffects.csv', 'BBCSoundEffects.csv')
     $Data = Import-Csv (Join-Path $Root 'BBCSoundEffects.csv')
-    ForEach($Item in $Data){
-        $Path = Join-Path $Root $Item.location
-        [System.Console]::WriteLine('Filename: ' + $Item.location + ', Category: ' + $Item.category)
-        $WebClient.DownloadFile(('http://bbcsfx.acropolis.org.uk/assets/' + $Item.location), $Path)
-    }
+    $Data | ForEach-Object -Parallel {
+        $Path = Join-Path $using:Root $_.location
+        [System.Console]::WriteLine('Filename: ' + $_.location + ', Category: ' + $_.category)
+        $Uri = ('http://bbcsfx.acropolis.org.uk/assets/' + $_.location)
+        if(Test-Path $_.location){
+            continue
+        } else{
+            $WebClient = [System.Net.WebClient]::New()
+            $WebClient.DownloadFile($Uri, $Path)
+        }
+        $WebClient.Dispose()
+    } -ThrottleLimit 6
 }
