2c1fd5ef52f55a6c0a53d64bb7b91e7cf8312f95
CatTheHacker <me@catthehacker.com>
2020-09-25 13:11:14 +0000

    Add backward compatibility with PS5 (synchronous downloads) and check if csv exists

--- bbcsfx.acropolis.org.uk/Get-BBCArchive.ps1
+++ bbcsfx.acropolis.org.uk/Get-BBCArchive.ps1
@@ -9,18 +9,34 @@ function Get-BBCArchive {
         $Root = (Get-Location).Path
     )
     $WebClient = [System.Net.WebClient]::New()
-    $WebClient.DownloadFile('http://bbcsfx.acropolis.org.uk/assets/BBCSoundEffects.csv', 'BBCSoundEffects.csv')
+    if(!(Test-Path (Join-Path $Root 'BBCSoundEffects.csv'))){
+        $WebClient.DownloadFile('http://bbcsfx.acropolis.org.uk/assets/BBCSoundEffects.csv', (Join-Path $Root 'BBCSoundEffects.csv'))
+    }
     $Data = Import-Csv (Join-Path $Root 'BBCSoundEffects.csv')
-    $Data | ForEach-Object -Parallel {
-        $Path = Join-Path $using:Root $_.location
-        [System.Console]::WriteLine('Filename: ' + $_.location + ', Category: ' + $_.category)
-        $Uri = ('http://bbcsfx.acropolis.org.uk/assets/' + $_.location)
-        if(Test-Path $_.location){
-            continue
-        } else{
-            $WebClient = [System.Net.WebClient]::New()
-            $WebClient.DownloadFile($Uri, $Path)
+    if($PSVersionTable.PSVersion.Major -gt 6){
+        $Data | ForEach-Object -Parallel {
+            $Path = Join-Path $using:Root $_.location
+            [System.Console]::WriteLine('Filename: ' + $_.location + ', Category: ' + $_.category)
+            $Uri = ('http://bbcsfx.acropolis.org.uk/assets/' + $_.location)
+            if(Test-Path $_.location){
+                continue
+            } else{
+                $WebClient = [System.Net.WebClient]::New()
+                $WebClient.DownloadFile($Uri, $Path)
+            }
+            $WebClient.Dispose()
+        } -ThrottleLimit $Threads
+    } else{
+        foreach($Item in $Data){
+            $Path = Join-Path $Root $Item.location
+            [System.Console]::WriteLine('Filename: ' + $Item.location + ', Category: ' + $Item.category)
+            $Uri = ('http://bbcsfx.acropolis.org.uk/assets/' + $Item.location)
+            if(Test-Path $Item.location){
+                continue
+            } else{
+                $WebClient.DownloadFile($Uri, $Path)
+            }
         }
-        $WebClient.Dispose()
-    } -ThrottleLimit $Threads
+    }
+    $WebClient.Dispose()
 }
